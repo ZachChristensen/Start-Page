73e048059278c25ebbeb602e95b93de920b4c57f
ZachChristensen <zachgchristensen@gmail.com>
2015-08-03 18:22:16 +1200

    links changed

--- Home.js
+++ Home.js
@@ -41,24 +41,24 @@ window.setInterval(function(){updateTime();}, 4000);
 
 //Links
 var link1 = document.getElementById("link1");
-link1.innerHTML = "4chan /m/";
-link1.href = "http://boards.4chan.org/m/";
+link1.innerHTML = "Google";
+link1.href = "https://www.google.com/";
  
 var link2 = document.getElementById("link2");
 link2.innerHTML = "Godzilla Guide";
 link2.href = "file:///C:/Users/Zach/Pictures/Guides/1399743712041.jpg";
  
 var link3 = document.getElementById("link3");
-link3.innerHTML = "4chan /a/";
-link3.href = "http://boards.4chan.org/a/";
- 
+link3.innerHTML = "Change links";
+link3.href = "https://www.google.com/";
+
 var link4 = document.getElementById("link4");
-link4.innerHTML = "8chan /a/";
-link4.href = "http://8ch.net/a/";
+link4.innerHTML = "and names";
+link4.href = "https://www.google.com/";
  
 var link5 = document.getElementById("link5");
-link5.innerHTML = "GundamGuy";
-link5.href = "http://gundamguy.blogspot.co.nz/";
+link5.innerHTML = "in .js file";
+link5.href = "https://www.google.com/";
  
 var link6 = document.getElementById("link6");
 link6.innerHTML = "YouTube";
@@ -77,25 +77,25 @@ link9.innerHTML = "Imgur";
 link9.href = "https://imgur.com/";
  
 var link10 = document.getElementById("link10");
-link10.innerHTML = "BakaBT";
-link10.href = "http://bakabt.me/";
- 
+link10.innerHTML = "Google";
+link10.href = "https://www.google.com/";
+
 var link11 = document.getElementById("link11");
-link11.innerHTML = "Weather";
-link11.href = "http://www.metservice.com/towns-cities/christchurch/christchurch";
+link11.innerHTML = "site";
+link11.href = "";
  
 var link12 = document.getElementById("link12");
-link12.innerHTML = "Nyaa.se";
-link12.href = "http://nyaa.se/";
+link12.innerHTML = "Google";
+link12.href = "https://www.google.com/";
  
 var link13 = document.getElementById("link13");
-link13.innerHTML = "MyAnimeList";
-link13.href = "http://myanimelist.net/panel.php";
+link13.innerHTML = "Google";
+link13.href = "https://www.google.com/";
 
 var link14 = document.getElementById("link14");
-link14.innerHTML = "R/a/dio";
-link14.href = "https://r-a-d.io/";
+link14.innerHTML = "Google";
+link14.href = "https://www.google.com/";
 
 var link15 = document.getElementById("link15");
-link15.innerHTML = "fufufu";
-link15.href = "https://fufufu.moe/a/";
+link15.innerHTML = "Google";
+link15.href = "https://www.google.com/";
